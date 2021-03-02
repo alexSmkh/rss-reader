e091b6eff61a95ba28a447e3981cf003a77d90d1
alexSmkh <alexshikari9333@gmail.com>
2021-03-02 13:43:04 +0300

    Changed state.valid to false on initialization

--- src/js/app.js
+++ src/js/app.js
@@ -87,7 +87,7 @@ export default () => {
 
       const state = {
         form: {
-          valid: true,
+          valid: false,
           processState: 'filling',
           fields: {
             input: '',
