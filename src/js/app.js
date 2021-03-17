c2cd98ca18f637b8a320409feef8b6423a7e9aa4
alexSmkh <alexshikari9333@gmail.com>
2021-03-17 16:41:01 +0300

    Changed the project structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,70 +1,11 @@
-import * as yup from 'yup';
 import i18next from 'i18next';
-import axios from 'axios';
 import _ from 'lodash';
 import resources from './locales/index.js';
 
 import initView from './view.js';
-import parseRss from './parser.js';
-import { normalizeURL, wrapUrlInCorsProxy } from './utils.js';
 import { buildPostListContainer, buildRssListContainer } from './components.js';
-
-const checkUpdates = (watchedState) => {
-  const timeoutDelay = 5000;
-  watchedState.rssSources.forEach((rssSource) => {
-    const proxyUrl = wrapUrlInCorsProxy(rssSource.link);
-    return axios
-      .get(proxyUrl)
-      .then((response) => {
-        const { posts: postsFromLastRequest } = parseRss(
-          response.data.contents,
-          'text/xml',
-        );
-        const newPostsFromLastRequest = _.differenceBy(
-          postsFromLastRequest,
-          watchedState.posts,
-          'link',
-        );
-        const newPosts = newPostsFromLastRequest.map((post) => ({
-          ...post,
-          id: _.uniqueId(),
-          sourceId: rssSource.id,
-        }));
-        watchedState.posts.unshift(...newPosts);
-      })
-      .catch()
-      .finally(() => {
-        if (watchedState.rssSources.length > 0) {
-          setTimeout(() => checkUpdates(watchedState), timeoutDelay);
-        }
-      });
-  });
-};
-
-const setValidationLocale = () => {
-  yup.setLocale({
-    string: {
-      url: 'errors.formValidation.url',
-    },
-    mixed: {
-      required: 'errors.formValidation.required',
-      notOneOf: 'errors.formValidation.rssAlreadyExists',
-    },
-  });
-};
-
-const validate = (urlList, url) => {
-  const schema = yup.object().shape({
-    url: yup.string().url().required().notOneOf(urlList),
-  });
-
-  try {
-    schema.validateSync({ url });
-    return null;
-  } catch (e) {
-    return e.errors[0];
-  }
-};
+import { handleFormInput, handleFormSubmit } from './handlers.js';
+import { setValidationLocale } from './validation.js';
 
 const initI18NextInstance = (lng) => {
   const i18nextInstance = i18next.createInstance();
@@ -74,7 +15,7 @@ const initI18NextInstance = (lng) => {
   });
 };
 
-const initElements = () => {
+const initDOMElements = () => {
   const submit = document.getElementById('add-content-btn');
   const input = document.getElementById('rss-input');
   const form = document.getElementById('rss-form');
@@ -92,8 +33,7 @@ const initElements = () => {
 };
 
 const runApp = (state, i18n) => {
-  const elements = initElements();
-  // console.log(elements);
+  const elements = initDOMElements();
   const watchedState = initView(state, elements, i18n);
   const { input, form, changeLangBtns } = elements;
 
@@ -105,76 +45,20 @@ const runApp = (state, i18n) => {
     });
   });
 
-  input.addEventListener('input', (e) => {
-    e.preventDefault();
-    const rssLink = e.target.value;
-    watchedState.form.fields.input = rssLink;
-    const existingRssLinks = watchedState.rssSources.map(
-      (rssSource) => rssSource.link,
-    );
-    const error = validate(existingRssLinks, rssLink);
-    watchedState.form.valid = !error;
-    watchedState.form.error = error;
-  });
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    watchedState.form.processState = 'sending';
-
-    const data = new FormData(e.target);
-    const rssLink = normalizeURL(data.get('rss-link'));
-    const proxyUrl = wrapUrlInCorsProxy(rssLink);
-    axios
-      .get(proxyUrl)
-      .then((response) => {
-        const parsedRss = parseRss(response.data.contents, 'text/xml');
-        watchedState.form.fields.input = '';
-        watchedState.form.processState = 'filling';
-        const newSource = {
-          ...parsedRss.source,
-          id: _.uniqueId(),
-          link: rssLink,
-        };
-        if (!watchedState.activeSourceId) {
-          watchedState.activeSourceId = newSource.id;
-        }
-
-        const postsOfNewSource = parsedRss.posts.map((post) => {
-          const id = _.uniqueId();
-          return {
-            ...post,
-            sourceId: newSource.id,
-            id,
-          };
-        });
-
-        watchedState.posts.push(...postsOfNewSource);
-        watchedState.rssSources.push(newSource);
-        if (watchedState.rssSources.length === 1) {
-          setTimeout(() => checkUpdates(watchedState), 5000);
-        }
-      })
-      .catch((err) => {
-        if (err.message === 'parse error') {
-          watchedState.form.error = 'errors.isNotSupported';
-          watchedState.form.processState = 'failed';
-          watchedState.form.valid = false;
-        }
-        if (err.message === 'Network Error') {
-          watchedState.form.processState = 'failed';
-          watchedState.error = err;
-        }
-      });
-  });
+  input.addEventListener('input', handleFormInput(watchedState));
+  form.addEventListener('submit', handleFormSubmit(watchedState, elements));
 };
 
 export default () => {
   setValidationLocale();
+
+  const defaultLanguage = 'en';
   const languages = ['en', 'ru'];
+
   const promises = languages.map(initI18NextInstance);
   return Promise.all(promises).then((i18nInstaces) => {
     const i18n = _.zipObject(languages, i18nInstaces);
-    const initialTranslateInstance = i18n.en;
+    const initialTranslateInstance = i18n[defaultLanguage];
     i18n.t = initialTranslateInstance;
 
     const state = {
@@ -190,7 +74,7 @@ export default () => {
       activeSourceId: null,
       posts: [],
       readPostIDs: new Set(),
-      language: 'en',
+      language: defaultLanguage,
     };
     runApp(state, i18n);
   });
---
+++ src/js/handlers.js
@@ -0,0 +1,112 @@
+import axios from 'axios';
+import _ from 'lodash';
+
+import parseRss from './parser.js';
+import { wrapUrlInCorsProxy, normalizeURL } from './utils.js';
+import { validate } from './validation.js';
+
+const checkUpdates = (watchedState) => {
+  const timeoutDelay = 5000;
+  watchedState.rssSources.forEach((rssSource) => {
+    const proxyUrl = wrapUrlInCorsProxy(rssSource.link);
+    return axios
+      .get(proxyUrl)
+      .then((response) => {
+        const { posts: postsFromLastRequest } = parseRss(
+          response.data.contents,
+          'text/xml',
+        );
+        const newPostsFromLastRequest = _.differenceBy(
+          postsFromLastRequest,
+          watchedState.posts,
+          'link',
+        );
+        const newPosts = newPostsFromLastRequest.map((post) => ({
+          ...post,
+          id: _.uniqueId(),
+          sourceId: rssSource.id,
+        }));
+        watchedState.posts.unshift(...newPosts);
+      })
+      .catch()
+      .finally(() => {
+        if (watchedState.rssSources.length > 0) {
+          setTimeout(() => checkUpdates(watchedState), timeoutDelay);
+        }
+      });
+  });
+};
+
+export const handleFormInput = (watchedState) => (e) => {
+  e.preventDefault();
+  const rssLink = e.target.value;
+  /* eslint-disable  no-param-reassign */
+  watchedState.form.fields.input = rssLink;
+  /* eslint-enable  no-param-reassign */
+  const existingRssLinks = watchedState.rssSources.map(
+    (rssSource) => rssSource.link,
+  );
+  const error = validate(existingRssLinks, rssLink);
+  /* eslint-disable  no-param-reassign */
+  watchedState.form.valid = !error;
+  watchedState.form.error = error;
+  /* eslint-enable  no-param-reassign */
+};
+
+export const handleFormSubmit = (watchedState) => (e) => {
+  e.preventDefault();
+  /* eslint-disable  no-param-reassign */
+  watchedState.form.processState = 'sending';
+  /* eslint-enable  no-param-reassign */
+
+  const data = new FormData(e.target);
+  const rssLink = normalizeURL(data.get('rss-link'));
+  const proxyUrl = wrapUrlInCorsProxy(rssLink);
+  axios
+    .get(proxyUrl)
+    .then((response) => {
+      const parsedRss = parseRss(response.data.contents, 'text/xml');
+      /* eslint-disable  no-param-reassign */
+      watchedState.form.fields.input = '';
+      watchedState.form.processState = 'filling';
+      /* eslint-enable  no-param-reassign */
+      const newSource = {
+        ...parsedRss.source,
+        id: _.uniqueId(),
+        link: rssLink,
+      };
+      if (!watchedState.activeSourceId) {
+        /* eslint-disable  no-param-reassign */
+        watchedState.activeSourceId = newSource.id;
+        /* eslint-enable  no-param-reassign */
+      }
+
+      const postsOfNewSource = parsedRss.posts.map((post) => {
+        const id = _.uniqueId();
+        return {
+          ...post,
+          sourceId: newSource.id,
+          id,
+        };
+      });
+
+      watchedState.posts.push(...postsOfNewSource);
+      watchedState.rssSources.push(newSource);
+      if (watchedState.rssSources.length === 1) {
+        setTimeout(() => checkUpdates(watchedState), 5000);
+      }
+    })
+    .catch((err) => {
+      if (err.message === 'parse error') {
+        /* eslint-disable  no-param-reassign */
+        watchedState.form.error = 'errors.isNotSupported';
+        watchedState.form.processState = 'failed';
+        watchedState.form.valid = false;
+      }
+      if (err.message === 'Network Error') {
+        watchedState.form.processState = 'failed';
+        watchedState.error = err;
+        /* eslint-enable  no-param-reassign */
+      }
+    });
+};
