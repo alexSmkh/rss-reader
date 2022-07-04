aaf8075e2c7704e8150e54022f47078b06a074e2
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-07-05 02:56:46 +0300

    Fixed proxy url

--- src/js/utils.js
+++ src/js/utils.js
@@ -7,7 +7,7 @@ export const normalizeURL = (url) => {
 };
 
 export const wrapUrlInCorsProxy = (rawURL) => {
-  const corsProxyURL = new URL('https://hexlet-allorigins.herokuapp.com/get');
+  const corsProxyURL = new URL('https://allorigins.hexlet.app/get');
   corsProxyURL.searchParams.append('url', rawURL);
   corsProxyURL.searchParams.append('disableCache', 'true');
   return corsProxyURL;
